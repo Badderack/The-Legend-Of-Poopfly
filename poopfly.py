1a86d38bf708c5831b11a7be7c1ec821d6deafb0
08SjoAnt <anton.sjogren@skola.taby.se>
2025-11-17 12:11:00 +0100

    Co-authored-by: Gr11s <Gr11s@users.noreply.github.com> Co-authored-by: liziarutinov <liziarutinov@users.noreply.github.com>

---
+++ poopfly.py
@@ -0,0 +1,23 @@
+from random import randint
+import time
+
+fnamn = ['Isak', 'Pelle', 'Ludvig', 'Anton', 'Lizi', 'Edmund']
+enamn = ['den fördärvade', 'Bajs', 'McMillen', 'Döden', 'O´ Moriah', 'Kall']
+
+class karaktar:
+    def __init__(karaktar, namn, kp, sty, niva, inventarie):
+        karaktar.namn = namn
+        karaktar.kp = kp
+        karaktar.sty = sty
+        karaktar.niva = niva
+        karaktar.inventarie = inventarie
+
+
+tempkp = randint(1, 5)
+tempsty = randint(5, 15)//tempkp
+sp1 = karaktar(f"{fnamn[randint(0, len(fnamn)-1)]} {enamn[randint(0, len(enamn)-1)]}", tempkp, tempsty, 0, 1)
+print(sp1.namn)
+print(sp1.kp)
+print(sp1.sty)
+
+while True:
---
+++ skatter.py
@@ -0,0 +1,5 @@
+class skatt:
+    def __init__(skatt, kvalitet, kpmod, stymod, lvlmod, annat):
+        skatt.kvalitet = kvalitet
+        skatt.kpmod = kpmod
+        skatt.stymod = stymod
